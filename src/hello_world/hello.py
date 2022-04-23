ea684d77f26e88b6c1fa7dd6d77b39ce1a7f126f
Mustansir Godhrawala <mgodhrawala402@gmail.com>
2022-04-23 17:11:20 +0530

    First commit

---
+++ src/hello_world/hello.py
@@ -0,0 +1,8 @@
+def print_hello():
+    print("Hello, World!")
+
+
+def custom_print(name):
+    print(f"Hello, {name}!")
+
+
---
+++ tests/test_hello_world.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import pytest
+from hello_world.hello import print_hello,custom_print
+
+
+def test_main(capfd):
+	print_hello()
+	out,err=capfd.readouterr()
+	assert out=="Hello, World!\n"
+
+
+def test_custom_main(capfd):
+	custom_print("Tom")
+	out,err=capfd.readouterr()
+	assert out=="Hello, Tom!\n"
